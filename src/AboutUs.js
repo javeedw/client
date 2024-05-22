2f448ec3b31f1133c5d5d17f54d7d9d9aaf83e01
javeedw <javeedw@yahoo.com>
2024-05-22 20:48:40 +1000

    About US Page commit

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -6,7 +6,7 @@ const { Title, Paragraph } = Typography;
 const AboutUs = () => {
   return (
     <div>
-      <Title>About Us Page</Title>
+      <Title>About Us Page test1</Title>
       <Paragraph>Learn more about us! - V3</Paragraph>
     </div>
   );
